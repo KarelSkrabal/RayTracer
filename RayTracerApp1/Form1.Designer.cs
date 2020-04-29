53d098b931dbd4d7b71524bd2321c36773cc6088
KarelSkrabal <karel.skrabal@seznam.cz>
2020-04-29 13:37:18 +0200

    UI form set-supports resizing

--- RayTracerApp1/Form1.Designer.cs
+++ RayTracerApp1/Form1.Designer.cs
@@ -48,32 +48,32 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.cmdDeleteSphere = new System.Windows.Forms.Button();
             this.grbSphere = new System.Windows.Forms.GroupBox();
-            this.txtColor = new System.Windows.Forms.TextBox();
             this.cmdEditSphere = new System.Windows.Forms.Button();
+            this.txtColor = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
+            this.cmdSaveSceneSettings = new System.Windows.Forms.Button();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.cmdSaveSceneSettings = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.cmdClose = new System.Windows.Forms.Button();
@@ -191,7 +191,8 @@
             // 
             // cmdStartRayTracing
             // 
-            this.cmdStartRayTracing.Location = new System.Drawing.Point(173, 500);
+            this.cmdStartRayTracing.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cmdStartRayTracing.Location = new System.Drawing.Point(7, 442);
             this.cmdStartRayTracing.Name = "cmdStartRayTracing";
             this.cmdStartRayTracing.Size = new System.Drawing.Size(130, 30);
             this.cmdStartRayTracing.TabIndex = 14;
@@ -201,8 +202,9 @@
             // 
             // cmdStopRayTracing
             // 
+            this.cmdStopRayTracing.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.cmdStopRayTracing.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.cmdStopRayTracing.Location = new System.Drawing.Point(309, 500);
+            this.cmdStopRayTracing.Location = new System.Drawing.Point(143, 442);
             this.cmdStopRayTracing.Name = "cmdStopRayTracing";
             this.cmdStopRayTracing.Size = new System.Drawing.Size(130, 30);
             this.cmdStopRayTracing.TabIndex = 15;
@@ -262,19 +264,12 @@
             this.grbSphere.Controls.Add(this.lbFixSphereY);
             this.grbSphere.Controls.Add(this.txtZ);
             this.grbSphere.Controls.Add(this.lbFixSphereX);
-            this.grbSphere.Location = new System.Drawing.Point(457, 13);
+            this.grbSphere.Location = new System.Drawing.Point(499, 13);
             this.grbSphere.Name = "grbSphere";
             this.grbSphere.Size = new System.Drawing.Size(790, 211);
             this.grbSphere.TabIndex = 19;
             this.grbSphere.TabStop = false;
             this.grbSphere.Text = "Sphere";
-            // 
-            // txtColor
-            // 
-            this.txtColor.Location = new System.Drawing.Point(69, 135);
-            this.txtColor.Name = "txtColor";
-            this.txtColor.Size = new System.Drawing.Size(100, 22);
-            this.txtColor.TabIndex = 14;
             // 
             // cmdEditSphere
             // 
@@ -285,6 +280,13 @@
             this.cmdEditSphere.Text = "Edit Sphere";
             this.cmdEditSphere.UseVisualStyleBackColor = true;
             this.cmdEditSphere.Click += new System.EventHandler(this.cmdEditSphere_Click);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(69, 135);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 22);
+            this.txtColor.TabIndex = 14;
             // 
             // groupBox1
             // 
@@ -312,140 +314,21 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(457, 240);
+            this.groupBox1.Location = new System.Drawing.Point(499, 240);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(787, 170);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Scene Settings";
             // 
-            // label1
+            // cmdSaveSceneSettings
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Canvas   Width";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 45);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Canvas Height";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Light Position   X";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(245, 78);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(17, 17);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Y";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 109);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(116, 17);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Light Direction  X";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(139, 17);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 6;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(139, 45);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 7;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(139, 75);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 8;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(268, 75);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 9;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(374, 77);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(17, 17);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "Z";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(397, 74);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 11;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(397, 105);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
-            this.textBox6.TabIndex = 16;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(374, 108);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(17, 17);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Z";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(268, 106);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
-            this.textBox7.TabIndex = 14;
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(139, 106);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 22);
-            this.textBox8.TabIndex = 13;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(245, 109);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(17, 17);
-            this.label9.TabIndex = 12;
-            this.label9.Text = "Y";
+            this.cmdSaveSceneSettings.Location = new System.Drawing.Point(503, 126);
+            this.cmdSaveSceneSettings.Name = "cmdSaveSceneSettings";
+            this.cmdSaveSceneSettings.Size = new System.Drawing.Size(130, 30);
+            this.cmdSaveSceneSettings.TabIndex = 23;
+            this.cmdSaveSceneSettings.Text = "Save";
+            this.cmdSaveSceneSettings.UseVisualStyleBackColor = true;
             // 
             // textBox9
             // 
@@ -495,14 +378,133 @@
             this.label11.TabIndex = 17;
             this.label11.Text = "Camera Position  X";
             // 
-            // cmdSaveSceneSettings
+            // textBox6
             // 
-            this.cmdSaveSceneSettings.Location = new System.Drawing.Point(503, 126);
-            this.cmdSaveSceneSettings.Name = "cmdSaveSceneSettings";
-            this.cmdSaveSceneSettings.Size = new System.Drawing.Size(130, 30);
-            this.cmdSaveSceneSettings.TabIndex = 23;
-            this.cmdSaveSceneSettings.Text = "Save";
-            this.cmdSaveSceneSettings.UseVisualStyleBackColor = true;
+            this.textBox6.Location = new System.Drawing.Point(397, 105);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(374, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Z";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(268, 106);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(139, 106);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(245, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Y";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(397, 74);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Z";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(268, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(139, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(139, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Light Direction  X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Light Position   X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Canvas Height";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Canvas   Width";
             // 
             // panel1
             // 
@@ -513,7 +515,7 @@
             this.panel1.Controls.Add(this.picScene);
             this.panel1.Location = new System.Drawing.Point(6, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(430, 431);
+            this.panel1.Size = new System.Drawing.Size(462, 358);
             this.panel1.TabIndex = 21;
             // 
             // groupBox2
@@ -522,9 +524,11 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.panel1);
+            this.groupBox2.Controls.Add(this.cmdStopRayTracing);
+            this.groupBox2.Controls.Add(this.cmdStartRayTracing);
             this.groupBox2.Location = new System.Drawing.Point(2, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(444, 461);
+            this.groupBox2.Size = new System.Drawing.Size(491, 491);
             this.groupBox2.TabIndex = 22;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -544,12 +548,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1256, 540);
+            this.ClientSize = new System.Drawing.Size(1298, 571);
             this.Controls.Add(this.cmdClose);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.cmdStopRayTracing);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.cmdStartRayTracing);
             this.Controls.Add(this.grbSphere);
             this.Controls.Add(this.lbProgress);
             this.Name = "Form1";
