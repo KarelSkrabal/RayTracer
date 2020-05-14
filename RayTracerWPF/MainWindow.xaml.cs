6d10b0184399efd611016e3cead5b16782a08ce7
KarelSkrabal <karel.skrabal@seznam.cz>
2020-05-15 00:08:50 +0200

    changing global font size,refactoring UI griddata

--- RayTracerWPF/MainWindow.xaml.cs
+++ RayTracerWPF/MainWindow.xaml.cs
@@ -34,16 +34,55 @@ namespace RayTracerWPF
             (sender as DataGrid).Columns[4].Header = "Point Y";
             (sender as DataGrid).Columns[5].Header = "Point Z";
             (sender as DataGrid).Columns[6].Header = "Color";
+
+            var columWidth = (sender as DataGrid).ActualWidth / 7;
+            (sender as DataGrid).Columns[0].Width = columWidth;
+            (sender as DataGrid).Columns[1].Width = columWidth;
+            (sender as DataGrid).Columns[2].Width = columWidth;
+            (sender as DataGrid).Columns[3].Width = columWidth;
+            (sender as DataGrid).Columns[4].Width = columWidth;
+            (sender as DataGrid).Columns[5].Width = columWidth;
+            (sender as DataGrid).Columns[6].Width = columWidth;
         }
 
         private void grdTriangles_Loaded(object sender, RoutedEventArgs e)
         {
+            (sender as DataGrid).Columns[0].Header = "Vertex1 X";
+            (sender as DataGrid).Columns[1].Header = "Vertex1 Y";
+            (sender as DataGrid).Columns[2].Header = "Vertex1 Z";
+            (sender as DataGrid).Columns[3].Header = "Vertex2 X";
+            (sender as DataGrid).Columns[4].Header = "Vertex2 Y";
+            (sender as DataGrid).Columns[5].Header = "Vertex2 Z";
+            (sender as DataGrid).Columns[6].Header = "Vertex3 X";
+            (sender as DataGrid).Columns[7].Header = "Vertex3 Y";
+            (sender as DataGrid).Columns[8].Header = "Vertex3 Z";
+            (sender as DataGrid).Columns[9].Header = "Color";
 
+            var columWidth = (sender as DataGrid).ActualWidth / 9;
+            (sender as DataGrid).Columns[0].Width = columWidth;
+            (sender as DataGrid).Columns[1].Width = columWidth;
+            (sender as DataGrid).Columns[2].Width = columWidth;
+            (sender as DataGrid).Columns[3].Width = columWidth;
+            (sender as DataGrid).Columns[4].Width = columWidth;
+            (sender as DataGrid).Columns[5].Width = columWidth;
+            (sender as DataGrid).Columns[6].Width = columWidth;
+            (sender as DataGrid).Columns[7].Width = columWidth;
+            (sender as DataGrid).Columns[8].Width = columWidth;
         }
 
         private void grdSpheres_Loaded(object sender, RoutedEventArgs e)
         {
+            (sender as DataGrid).Columns[1].Header = "Center X";
+            (sender as DataGrid).Columns[2].Header = "Center Y";
+            (sender as DataGrid).Columns[3].Header = "Center Z";
+            (sender as DataGrid).Columns[4].Header = "Color";
 
+            var columWidth = (sender as DataGrid).ActualWidth / 4;
+            (sender as DataGrid).Columns[0].Width = columWidth;
+            (sender as DataGrid).Columns[1].Width = columWidth;
+            (sender as DataGrid).Columns[2].Width = columWidth;
+            (sender as DataGrid).Columns[3].Width = columWidth;
+            (sender as DataGrid).Columns[4].Width = columWidth;
         }
     }
 }
