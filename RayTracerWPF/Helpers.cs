dd84a650386c496f0e33185caba11948e77d5922
KarelSkrabal <karel.skrabal@seznam.cz>
2020-05-15 12:21:46 +0200

    pickColor cmd implemented (no generic)

--- RayTracerWPF/Helpers.cs
+++ RayTracerWPF/Helpers.cs
@@ -10,5 +10,13 @@ namespace RayTracerWPF
     public static class Helpers
     {
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> col) => new ObservableCollection<T>(col);
+        public static IEnumerable<T> SetValue<T>(this IEnumerable<T> items, Action<T> updateMethod)
+        {
+            foreach (T item in items)
+            {
+                updateMethod(item);
+            }
+            return items;
+        }
     }
 }
--- RayTracerWPF/ViewModel/MainViewModel.cs
+++ RayTracerWPF/ViewModel/MainViewModel.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms;
 using RayTracerWPF.Model;
 using RayTracerWPF.MVVMCodeBase;
 using System.Collections.ObjectModel;
@@ -5,11 +6,73 @@ using System.Windows.Input;
 using Plane = RayTracerWPF.Model.Plane;
 using Sphere = RayTracerWPF.Model.Sphere;
 using Triangle = RayTracerWPF.Model.Triangle;
+using System.Windows.Media;
 
 namespace RayTracerWPF.ViewModel
 {
     public class MainViewModel
     {
+        private ColorDialog dlgColor = new ColorDialog();
+        public ICommand cmdPlanePickColor { get { return new RelayCommand(PlanePickColorExecute, CanPlanePickColorExecute); } }
+
+        private bool CanPlanePickColorExecute() => SelectedPlane != null && Planes.Contains(SelectedPlane);
+
+        private void PlanePickColorExecute()
+        {
+            int index = Planes.IndexOf(SelectedPlane);
+
+            if (dlgColor.ShowDialog() == DialogResult.OK)
+            {
+                Planes[index].Color = dlgColor.Color.Name;
+                SelectedPlane = null;
+            }
+        }
+
+        public ICommand cmdTrianglePickColor { get { return new RelayCommand(TrianglePickColorExecute, CanTrianglePickColorExecute); } }
+
+        private bool CanTrianglePickColorExecute() => SelectedTriangle != null && Triangles.Contains(SelectedTriangle);
+
+        private void TrianglePickColorExecute()
+        {
+            int index = Triangles.IndexOf(SelectedTriangle);
+
+            if (dlgColor.ShowDialog() == DialogResult.OK)
+            {
+                Triangles[index].Color = dlgColor.Color.Name;
+                SelectedTriangle = null;
+            }
+        }
+        public ICommand cmdSpherePickColor { get { return new RelayCommand(SpherePickColorExecute, CanSpherePickColorExecute); } }
+
+        private bool CanSpherePickColorExecute() => SelectedSphere != null && Spheres.Contains(SelectedSphere);
+
+        private void SpherePickColorExecute()
+        {
+            int index = Spheres.IndexOf(SelectedSphere);
+
+            if (dlgColor.ShowDialog() == DialogResult.OK)
+            {
+                Spheres[index].Color = dlgColor.Color.Name;
+                SelectedSphere = null;
+            }
+        }
+
+        //TODO-implement generic command
+        private void PlanePickColorExecute<T>(ObservableCollection<T> collection, T item)
+        {
+            int index = collection.IndexOf(item);
+            if (dlgColor.ShowDialog() == DialogResult.OK)
+            {
+                (collection[index] as RayTracerLib.BaseObject).color = new RayTracerLib.Color
+                {
+                    R = dlgColor.Color.R,
+                    G = dlgColor.Color.G,
+                    B = dlgColor.Color.B
+                };
+                SelectedPlane = null;
+            }
+        }
+
         public MainViewModel()
         {
             _spheres.Add(new Sphere { Radius = 45, CenterX = 71, CenterY = 72, CenterZ = 73, Color = "sphere color" });
@@ -46,6 +109,9 @@ namespace RayTracerWPF.ViewModel
         private ObservableCollection<Triangle> _triangles = new ObservableCollection<Triangle>();
         private ObservableCollection<Sphere> _spheres = new ObservableCollection<Sphere>();
         private Scene _scene = new Scene();
+        private Plane _selectedPlane = new Plane();
+        private Triangle _selectedTriangle = new Triangle();
+        private Sphere _selectedSphere = new Sphere();
 
         public ObservableCollection<Plane> Planes
         {
@@ -71,6 +137,23 @@ namespace RayTracerWPF.ViewModel
             set => _spheres = value;
         }
 
+        public Plane SelectedPlane
+        {
+            get => _selectedPlane;
+            set => _selectedPlane = value;
+        }
+
+        public Triangle SelectedTriangle
+        {
+            get => _selectedTriangle;
+            set => _selectedTriangle = value;
+        }
+
+        public Sphere SelectedSphere
+        {
+            get => _selectedSphere;
+            set => _selectedSphere = value;
+        }
         public ICommand AddSphere { get { return new RelayCommand(AddSphereExecute, CanAddSphereExecute); } }
 
         private bool CanAddSphereExecute() => true;
