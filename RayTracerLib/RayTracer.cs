ad076530525a441f478e480bfea93b9414439978
KarelSkrabal <karel.skrabal@seznam.cz>
2020-05-17 23:37:17 +0200

    Ray Tracer

--- RayTracerLib/RayTracer.cs
+++ RayTracerLib/RayTracer.cs
@@ -24,7 +24,7 @@ namespace RayTracerLib
 
         public void RayTraceScene(ref Color[,] pixels)
         {
-            InitializeOutput(ref pixels);            
+            InitializeOutput(ref pixels);
             for (int i = 0; i < canvasWidth; i++)
             {
                 for (int j = 0; j < canvasHeight; j++)
@@ -84,17 +84,23 @@ namespace RayTracerLib
             canvasHeight = 600;
             canvasWidth = 600;
             lp = new Vector(0.0, 100.0, 300.0);
-            lv = new Vector(-1.0, 1.0, -1.0);
-            p = new Vector(100.0, 50.0, 500.0);
+            lv = new Vector(1.0, 1.0, -0.5);
+            p = new Vector(100.0, 100.0, 500.0);
             objects = new List<BaseObject>();
             //_rayTracer.objects.Add(new Plane(new Vector(10.0, 10.0, -10), new Vector(10.0, 10.0, 10.0), new RayTracerLib.Color(255.0, 0.0, 0.0)));
-            objects.Add(new Sphere(60.0, 60.0, 0.0, 60.0, new RayTracerLib.Color(0.0, 0.0, 255.0)));
-            objects.Add(new Sphere(300.0, 300.0, 0.0, 40.0, new RayTracerLib.Color(255.0, 0.0, 255.0)));
+            objects.Add(new Sphere(200.0, 100.0, 0.0, 40.0, new Color(0.0, 0.0, 255.0)));
+            objects.Add(new Sphere(300.0, 150.0, -50.0, 60.0, new Color(255.0, 0.0, 255.0)));
+            objects.Add(new Sphere(100.0, 100.0, 180.0, 60.0, new Color(255.0, 0.0, 255.0)));
             Vector v0 = new Vector(100.0, 100.0, 0.0);
             Vector v1 = new Vector(100.0, 80.0, 0.0);
             Vector v2 = new Vector(80.0, 100.0, 0.0);
-            Triangle tt = new Triangle(v0, v1, v2, new RayTracerLib.Color(200.0, 30.0, 30.0));
-            objects.Add(tt);
+            Triangle t = new Triangle(v0, v1, v2, new Color(255.0, 0.0, 0.0));
+            objects.Add(t);
+            v0 = new Vector(200.0, 200.0, 0.0);
+            v1 = new Vector(200.0, 280.0, 0.0);
+            v2 = new Vector(280.0, 300.0, 0.0);
+            t = new Triangle(v0, v1, v2, new Color(0.0, 255.0, 0.0));
+            objects.Add(t);
         }
 
         /// <summary>
